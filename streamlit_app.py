91b7c08f641cc0b43a2bfcaa68f98eb170541e88
zds78 <zds78@126.com>
2022-07-19 16:52:35 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,15 @@ import math
 import pandas as pd
 import streamlit as st
 
+st.set_page_config(
+    page_title="Hello",
+    page_icon="👋",
+)
+
+st.write("# Welcome to Streamlit! 👋")
+
+st.sidebar.success("Select a demo above.")
+
 """
 # Welcome to Streamlit!
 
