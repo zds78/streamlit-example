222d8df7b46221681b77d65cee0d54c0d63c51e0
zds78 <zds78@126.com>
2022-07-21 12:12:07 +0800

    Update 1_page1.py

--- pages/1_page1.py
+++ pages/1_page1.py
@@ -5,3 +5,12 @@ import numpy as np
 st.sidebar.success("sl1...")
 
 st.title('zds page 11111')
+
+
+img_file = st.sidebar.file_uploader(label='Upload a file', type=['png', 'jpg'])
+if img_file:
+    bytes_data = img_file.getvalue()
+    fl = open(img_file.name, 'wb')
+    fl.write(bytes_data)
+    fl.close()
+
